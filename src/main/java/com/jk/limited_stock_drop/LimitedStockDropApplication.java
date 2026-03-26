560d16065a437d89be9796866f614d8d8b68d8b9
Jasurbek Khamroev <khamroevapple@gmail.com>
2026-03-26 09:42:50 +0100

    Initial Commit

---
+++ src/main/java/com/jk/limited_stock_drop/LimitedStockDropApplication.java
@@ -0,0 +1,13 @@
+package com.jk.limited_stock_drop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedStockDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedStockDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/limited_stock_drop/LimitedStockDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.limited_stock_drop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedStockDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
